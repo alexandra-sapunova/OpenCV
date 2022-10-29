b2faa821ef5cebbf165e9e7bdebd2ae8421569f7
Alexandra Sapunova <aleksandrasapunova11@gmail.com>
2022-10-29 09:45:25 +0300

    Task 1.5

--- lab1/main.py
+++ lab1/main.py
@@ -57,3 +57,37 @@ def read_video_write_to_file():
 
     video.release()
     cv2.destroyAllWindows()
+
+# task 5
+def read_ip_write_to_file():
+    video = cv2.VideoCapture(0)
+    w = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    h = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    video_writer = cv2.VideoWriter("output.mov", fourcc, 25, (w, h))
+
+    while True:
+        ok, img = video.read()
+        cv2.imshow('img', img)
+        video_writer.write(img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    video.release()
+    cv2.destroyAllWindows()
+
+
+def print_cam():
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 640)
+    cap.set(4, 480)
+    while True:
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        cv2.imshow('frame', gray)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
