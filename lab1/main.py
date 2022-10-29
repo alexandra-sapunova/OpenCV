40ee2d93694ba8e9bdb9be2fcb76b73a6536c78e
Alexandra Sapunova <aleksandrasapunova11@gmail.com>
2022-10-29 09:43:06 +0300

    Task 1.2

--- lab1/main.py
+++ lab1/main.py
@@ -3,4 +3,12 @@ import numpy as np
 import requests
 import imutils
 
+# task 2
+def read_photo():
+    img = cv2.imread(r'C:\Users\aleks\Downloads\1604650136_1.jpg')
+    cv2.namedWindow('Display window', cv2.WINDOW_AUTOSIZE)
+    cv2.imshow('Display window', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 
