375ac2156db4f746b7395ed503f341a65e1a32cd
Alexandra Sapunova <aleksandrasapunova11@gmail.com>
2022-10-29 09:43:35 +0300

    Task 1.3

--- lab1/main.py
+++ lab1/main.py
@@ -12,3 +12,26 @@ def read_photo():
     cv2.destroyAllWindows()
 
 
+# task 3
+# COLOR_RGB2HSV   COLOR_BGR2HSV
+def read_video():
+    cap = cv2.VideoCapture(r'C:\Users\aleks\Downloads\Остров сокровищ мем  Доктор Ливси мем. Dr. livesey meme 4k..mp4', cv2.CAP_ANY)
+    cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("frame", 500, 500)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        cv2.imshow('frame', frame)
+
+        # Черно-белое
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # cv2.imshow('frame', gray)
+
+        if cv2.waitKey(10) & 0xFF == 27:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
